964572931840e612dc1f01217850a52e9db0a8e8
AugustAtSeattle <august.lin.usa@gmail.com>
2024-08-23 13:40:41 -0700

    Initial Commit

---
+++ OpenWeatherApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  OpenWeatherApp
+//
+//  Created by Sailor on 8/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
