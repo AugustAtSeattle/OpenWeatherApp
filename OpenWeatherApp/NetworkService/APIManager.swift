1a2236fe6da156b78e3df7b5567c889d522a41d9
AugustAtSeattle <august.lin.usa@gmail.com>
2024-09-16 11:39:33 -0700

    warning, expose API Key

--- OpenWeatherApp/NetworkService/APIManager.swift
+++ OpenWeatherApp/NetworkService/APIManager.swift
@@ -19,7 +19,8 @@ class APIManager: APIManagerProtocol {
 
     static let shared = APIManager()
         
-    
+    private let apiKey = "8465f8f701c13e008c92b94e893ab308"
+
     // Step 1: Obtain geographic coordinates using the Geocoding API
     func getCoordinates(for cityName: String, completion: @escaping (Result<Coordinate, NetworkError>) -> Void) {
         let endpoint = Endpoint.geocode(cityName: cityName, apiKey: apiKey)
